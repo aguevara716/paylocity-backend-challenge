a123f6b03a8d1c12db2d0593407c12ba710a4e7e
Angel Guevara, Jr <aguevara.716@gmail.com>
2024-02-29 13:17:33 -0500

    Added more paycheck generation tests, handling error cases

--- PaylocityBenefitsCalculator/ApiTests/IntegrationTests/PaycheckIntegrationTests.cs
+++ PaylocityBenefitsCalculator/ApiTests/IntegrationTests/PaycheckIntegrationTests.cs
@@ -18,4 +18,26 @@ public sealed class PaycheckIntegrationTests : IntegrationTest
 
         await response.ShouldReturn(HttpStatusCode.OK);
     }
+
+    [Fact]
+    public async Task Get_Should_ReturnErrorIfPaycheckIsInFuture()
+    {
+        var checkId = 100;
+        var employeeId = 1;
+
+        var response = await HttpClient.GetAsync($"/api/v1/paychecks/{checkId}/employee/{employeeId}");
+
+        await response.ShouldReturn(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Get_Should_ReturnErrorIfEmployeeDoesNotExist()
+    {
+        var checkId = 26;
+        var employeeId = int.MaxValue;
+
+        var response = await HttpClient.GetAsync($"/api/v1/paychecks/{checkId}/employee/{employeeId}");
+
+        await response.ShouldReturn(HttpStatusCode.BadRequest);
+    }
 }
