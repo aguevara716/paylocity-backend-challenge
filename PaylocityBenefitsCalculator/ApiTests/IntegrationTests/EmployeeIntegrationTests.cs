2c2f6501700c9fb2234a5affada81599d43d027d
Angel Guevara, Jr <aguevara.716@gmail.com>
2024-02-29 13:17:11 -0500

    Fixed flaky tests
    They were failing because other tests create employees/dependents, and the original GET tests expected specific employees/dependents

--- PaylocityBenefitsCalculator/ApiTests/IntegrationTests/EmployeeIntegrationTests.cs
+++ PaylocityBenefitsCalculator/ApiTests/IntegrationTests/EmployeeIntegrationTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -7,100 +8,112 @@ using Api.Dtos.Dependent;
 using Api.Dtos.Employee;
 using Api.Models;
 using ApiTests.Extensions;
+using FluentAssertions;
 using Xunit;
 
 namespace ApiTests.IntegrationTests;
 
 public sealed class EmployeeIntegrationTests : IntegrationTest
 {
-    // GET
-    [Fact]
-    public async Task WhenAskedForAllEmployees_ShouldReturnAllEmployees()
+    private readonly static List<GetEmployeeDto> employees = new()
     {
-        var response = await HttpClient.GetAsync("/api/v1/employees");
-        var employees = new List<GetEmployeeDto>
-        {
-            new()
-            {
-                Id = 1,
-                FirstName = "LeBron",
-                LastName = "James",
-                Salary = 75420.99m,
-                DateOfBirth = new DateTime(1984, 12, 30)
-            },
-            new()
-            {
-                Id = 2,
-                FirstName = "Ja",
-                LastName = "Morant",
-                Salary = 92365.22m,
-                DateOfBirth = new DateTime(1999, 8, 10),
-                Dependents = new List<GetDependentDto>
-                {
-                    new()
-                    {
-                        Id = 1,
-                        FirstName = "Spouse",
-                        LastName = "Morant",
-                        Relationship = Relationship.Spouse,
-                        DateOfBirth = new DateTime(1998, 3, 3)
-                    },
-                    new()
-                    {
-                        Id = 2,
-                        FirstName = "Child1",
-                        LastName = "Morant",
-                        Relationship = Relationship.Child,
-                        DateOfBirth = new DateTime(2020, 6, 23)
-                    },
-                    new()
-                    {
-                        Id = 3,
-                        FirstName = "Child2",
-                        LastName = "Morant",
-                        Relationship = Relationship.Child,
-                        DateOfBirth = new DateTime(2021, 5, 18)
-                    }
-                }
-            },
-            new()
-            {
-                Id = 3,
-                FirstName = "Michael",
-                LastName = "Jordan",
-                Salary = 143211.12m,
-                DateOfBirth = new DateTime(1963, 2, 17),
-                Dependents = new List<GetDependentDto>
-                {
-                    new()
-                    {
-                        Id = 4,
-                        FirstName = "DP",
-                        LastName = "Jordan",
-                        Relationship = Relationship.DomesticPartner,
-                        DateOfBirth = new DateTime(1974, 1, 2)
-                    }
-                }
-            }
-        };
-        await response.ShouldReturn(HttpStatusCode.OK, employees);
-    }
-
-    [Fact]
-    public async Task WhenAskedForAnEmployee_ShouldReturnCorrectEmployee()
-    {
-        var response = await HttpClient.GetAsync("/api/v1/employees/1");
-        var employee = new GetEmployeeDto
+        new()
         {
             Id = 1,
             FirstName = "LeBron",
             LastName = "James",
             Salary = 75420.99m,
             DateOfBirth = new DateTime(1984, 12, 30)
-        };
-        await response.ShouldReturn(HttpStatusCode.OK, employee);
+        },
+        new()
+        {
+            Id = 2,
+            FirstName = "Ja",
+            LastName = "Morant",
+            Salary = 92365.22m,
+            DateOfBirth = new DateTime(1999, 8, 10),
+            Dependents = new List<GetDependentDto>
+            {
+                new()
+                {
+                    Id = 1,
+                    FirstName = "Spouse",
+                    LastName = "Morant",
+                    Relationship = Relationship.Spouse,
+                    DateOfBirth = new DateTime(1998, 3, 3)
+                },
+                new()
+                {
+                    Id = 2,
+                    FirstName = "Child1",
+                    LastName = "Morant",
+                    Relationship = Relationship.Child,
+                    DateOfBirth = new DateTime(2020, 6, 23)
+                },
+                new()
+                {
+                    Id = 3,
+                    FirstName = "Child2",
+                    LastName = "Morant",
+                    Relationship = Relationship.Child,
+                    DateOfBirth = new DateTime(2021, 5, 18)
+                }
+            }
+        },
+        new()
+        {
+            Id = 3,
+            FirstName = "Michael",
+            LastName = "Jordan",
+            Salary = 143211.12m,
+            DateOfBirth = new DateTime(1963, 2, 17),
+            Dependents = new List<GetDependentDto>
+            {
+                new()
+                {
+                    Id = 4,
+                    FirstName = "DP",
+                    LastName = "Jordan",
+                    Relationship = Relationship.DomesticPartner,
+                    DateOfBirth = new DateTime(1974, 1, 2)
+                }
+            }
+        }
+    };
+
+    // GET
+    [Fact]
+    public async Task WhenAskedForAllEmployees_ShouldReturnAllEmployees()
+    {
+        var expectedEmployees = employees;
+
+        var response = await HttpClient.GetAsync("/api/v1/employees");
+
+        response.Should().HaveStatusCode(HttpStatusCode.OK);
+        var actualEmployeesResponse = await response.Content.DeserializeTo<ApiResponse<List<GetEmployeeDto>>>();
+        actualEmployeesResponse!.Success.Should().BeTrue();
+        var actualEmployees = actualEmployeesResponse!.Data!;
+        foreach (var expectedEmployee in expectedEmployees)
+        {
+            var actualEmployee = actualEmployees.FirstOrDefault(e => e.Id == expectedEmployee.Id);
+            actualEmployee.Should().NotBeNull().And.BeEquivalentTo(expectedEmployee);
+        }
     }
-    
+
+    [Fact]
+    public async Task WhenAskedForAnEmployee_ShouldReturnCorrectEmployee()
+    {
+        var employeeId = 3;
+        var expectedEmployee = employees.FirstOrDefault(e => e.Id == employeeId);
+
+        var response = await HttpClient.GetAsync($"/api/v1/employees/{employeeId}");
+
+        response.Should().HaveStatusCode(HttpStatusCode.OK);
+        var actualEmployeeResponse = await response.Content.DeserializeTo<ApiResponse<Employee>>();
+        var actualEmployee = actualEmployeeResponse!.Data!;
+        actualEmployee.Should().NotBeNull().And.BeEquivalentTo(expectedEmployee);
+    }
+
     [Fact]
     public async Task WhenAskedForANonexistentEmployee_ShouldReturn404()
     {
@@ -163,4 +176,3 @@ public sealed class EmployeeIntegrationTests : IntegrationTest
         await response.ShouldReturn(HttpStatusCode.BadRequest);
     }
 }
-
